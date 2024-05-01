60ed9ccc4a6f827e458da2d0d9077147ad44ce95
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-05-01 19:17:49 +0300

    Adding decomentation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ */
 
 void bubble_sort(int *array, size_t size)
 {
