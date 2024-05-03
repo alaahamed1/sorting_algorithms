f9bebbf1150b182c4801bfbfb19f242080d513b9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-05-03 10:53:39 +0300

    Task-2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,29 +9,29 @@
 
 void selection_sort(int *array, size_t size)
 {
-	int i, j;
+	size_t i, j, min;
 	int temp;
-	int min = i;
 
 	if (!array || !size)
 	{
 		return;
 	}
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = i + 1; j < size - 1 - i; j++)
+		min = i;
+		for (j = i; j < size; j++)
 		{
-			if (array[j] < array[min])
+			if (array[min] > array[j])
 			{
 				min = j;
 			}
 		}
-	}
-	if (min != i)
-	{
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 	}
 }
