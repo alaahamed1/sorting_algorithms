ef5aba2407b5668bf7f3b685357a08dc852424ef
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-05-05 03:15:24 +0300

    Task-4 Advanced

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,41 +6,29 @@
  * @size: size of the array
  * Return: initial gap value
  */
-size_t calculate_initial_gap(size_t size)
-{
-	size_t gap = 1;
-
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	return gap;
-}
-
-/**
- * shell_sort - sorts an array of integers in ascending order
- * using the Shell sort algorithm with Knuth sequence
- * @array: array to be sorted
- * @size: size of the array
- */
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = calculate_initial_gap(size);
-	size_t i, j;
-	int temp;
+	int gap;
 
-	while (gap > 0)
+	for (gap = size / 3; gap > 0; gap /= 3)
 	{
-		printf("Gap = %lu\n", gap);
-		for (i = gap; i < size; i++)
+		gap = (gap % 2 == 0) ? gap + 1 : gap;
+		for (int i = gap; i < size; i++)
 		{
-			temp = array[i];
-			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			int temp = array[i];
+			int j = i;
+			while (j >= gap && array[j - gap] > temp)
 			{
 				array[j] = array[j - gap];
+				j -= gap;
 			}
 			array[j] = temp;
 		}
-		print_array(array, size);
-		gap = (gap - 1) / 3;
+		printf("Array after gap %d: ", gap);
+		for (int i = 0; i < size; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
 	}
 }
